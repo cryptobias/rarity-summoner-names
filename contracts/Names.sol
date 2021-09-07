6ef79ddc439707b28ed63b656155788f33e0a388
cryptobias <cryptobias@protonmail.com>
2021-09-07 20:04:12 +0200

    Require an exponentially rising price for renames
    To allow chaning the name, but give an incentive to not do it all
    the time.

--- contracts/Names.sol
+++ contracts/Names.sol
@@ -5,13 +5,38 @@ import "./Rarity.sol";
 import "./RarityGold.sol";
 
 contract Names {
+    // start at the low cost of 100 gold
+    uint initialRenameCost = 100e18;
+
     Rarity public rarity = Rarity(0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb);
     RarityGold public rarityGold = RarityGold(0x2069B76Afe6b734Fb65D1d099E7ec64ee9CC76B2);
 
+    uint public burn; 
     mapping(uint => string) public names;
+    mapping(uint => uint) public nextRenameCost;
+
+    constructor() {
+        // summon a sorcerer to burn gold that is paid for renames
+        burn = rarity.next_summoner();
+        rarity.summon(10);
+    }
 
     function setName(uint summoner, string memory name) external {
         require(msg.sender == rarity.ownerOf(summoner), "only the owner can set the name of a summoner");
+        require(rarityGold.allowance(summoner, burn) >= nextRenameCost[summoner], "insufficient allowance for rename");
+        require(rarityGold.balanceOf(summoner) >= nextRenameCost[summoner], "insufficient gold for rename");
+
+        // store the name
         names[summoner] = name;
+
+        // calculate the next cost and burt it if necessary
+        uint renameCost = nextRenameCost[summoner];
+        if(renameCost == 0) {
+            nextRenameCost[summoner] = initialRenameCost;
+        } else {
+            // exponentially increase the costs
+            nextRenameCost[summoner] = nextRenameCost[summoner] * 2;
+            rarityGold.transferFrom(burn, summoner, burn, renameCost);
+        }
     }
 }
--- test/test.js
+++ test/test.js
@@ -1,8 +1,11 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { ethers, network } = require("hardhat");
 
 describe("Names", function () {
   let names;
+  let summoner;
+  let rarity;
+  let rarityGold;
 
   before(async function() {
     // deploy the contract
@@ -10,20 +13,83 @@ describe("Names", function () {
     names = await Names.deploy();
     await names.deployed();
 
-    // load the rarity contract  
-    const rarity = await ethers.getContractAt("Rarity", await names.rarity());
-    
+    expect(await names.burn()).to.not.equal(0);
+
+    // load the rarity contracts
+    rarity = await ethers.getContractAt("Rarity", await names.rarity());
+    rarityGold = await ethers.getContractAt("RarityGold", await names.rarityGold());
+
     // summon an adventure to set the name on
     summoner = await rarity.next_summoner();
     await rarity.summon(11);
+
+    // adventure until level up is available
+    const requiredXP = await rarity.xp_required(1);
+    let currentXP = await rarity.xp(summoner);
+
+    while(currentXP.lt(requiredXP)) {
+      // adventure for XP
+      await rarity.adventure(summoner);
+      currentXP = await rarity.xp(summoner);
+
+      // turn the time forward by a day
+      await network.provider.send("evm_increaseTime", [60 * 60 * 24 + 60]);
+    }
+
+    // level up
+    await rarity.level_up(summoner);
   });
 
   it("should set a summoners name", async function() {
      // set the summoner's name
     await names.setName(summoner, "Harry")
-    
+
     // expect the name to be stored
     await expect(await names.names(summoner)).to.equal("Harry");
+  });
+
+  it("should NOT allow to change the name if burn hasn't been approved sufficient funds", async function() {
+    // try to rename the summoner and expect it to fail
+    await expect(
+      names.setName(summoner, "Ron")
+    ).to.be.revertedWith("insufficient allowance for rename");
+  });
+
+  it("should NOT allow to change the name without sufficient funds", async function() {
+    // allow the rename cost to get burned
+    await rarityGold.approve(
+      summoner,
+      await names.burn(),
+      await names.nextRenameCost(summoner)
+    );
+
+    // try to rename the summoner and expect it to fail
+    await expect(
+      names.setName(summoner, "Ron")
+    ).to.be.revertedWith("insufficient gold for rename");
+  });
+
+  it("should allow to change the name with sufficient funds", async function() {
+    // remember rename cost
+    const renameCostBefore = await names.nextRenameCost(summoner)
+
+    // claim the summoners gold
+    rarityGold.claim(summoner);
+    const goldBefore = await rarityGold.balanceOf(summoner);
+
+    // rename the summoner
+    await names.setName(summoner, "Ron")
+
+    // expect the new name to be stored
+    await expect(await names.names(summoner)).to.equal("Ron");
+
+    // expect the summoner's gold to have decreased
+    const goldAfter = await rarityGold.balanceOf(summoner);
+    await expect(goldAfter).to.be.lt(goldBefore);
+
+    // expect the rename cost to have gone up
+    const renameCostAfter = await names.nextRenameCost(summoner)
+    await expect(renameCostAfter).to.be.gt(renameCostBefore);
   });
 
   it("should NOT allow to set a non-owned summoners name", async function() {
