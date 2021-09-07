18f5aefd67d8e32dc194ae69d89664c8593c2431
cryptobias <cryptobias@protonmail.com>
2021-09-07 21:43:23 +0200

    Introduce interfaces for rarity contracts
    This way it is not necessary to keep the contracts in this repo
    and they do not need to provided during verification on ftmscan.

--- contracts/Names.sol
+++ contracts/Names.sol
@@ -1,8 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "./Rarity.sol";
-import "./RarityGold.sol";
+interface Rarity {
+    function ownerOf(uint256 tokenId) external view returns (address owner);
+    
+    // for tests
+    function adventure(uint _summoner) external;
+    function level_up(uint _summoner) external;
+    function next_summoner() external view returns (uint summoner);
+    function summon(uint _class) external;
+    function xp_required(uint curent_level) external view returns (uint xp_to_next_level);
+    function xp(uint _summoner) external view returns (uint xp);
+}
+
+interface RarityGold {
+    function allowance(uint from, uint spender) external view returns (uint amount);
+    function balanceOf(uint summoner) external view returns (uint amount);
+    function transferFrom(uint executor, uint from, uint to, uint amount) external returns (bool);
+
+    // for tests
+    function approve(uint from, uint spender, uint amount) external returns (bool);
+    function claim(uint summoner) external;
+}
 
 contract Names {
     // start at the low cost of 100 gold
